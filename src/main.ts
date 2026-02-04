9e038d935c786c9e897f765b207c1c11f32d62f1
Duane Enriquez <duanequez@gmail.com>
2026-02-04 16:57:41 +0800

    add logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('DEVELOPER')
   const app = await NestFactory.create(AppModule);
+  logger.log("Testing")
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
